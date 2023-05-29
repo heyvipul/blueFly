884c6718894fc89e2873d1dd356ce05489cb4b93
Vikas Chaturvedi <vc7068318009@gmail.com>
2023-05-29 15:35:40 +0530

    womenzpp

---
+++ SCRIPTS/menproduct.js
@@ -0,0 +1,350 @@
+var  mensdata=[
+
+    {  brand_name:"Louis Philip",
+ productType:"mansclothing",
+      name:"LP Brand - Symbol Men Shirt",
+     image_url:"https://m.media-amazon.com/images/I/A1wEjvjUA-L._AC_UL480_FMwebp_QL65_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/A1yExRGrofL._AC_UX679_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/91ELg13MgUL._AC_UX679_.jpg",
+     strikedoffprice: "799",
+     price: "557",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"Louis Philip",
+ productType:"mansclothing",
+      name:"LP Brand - Symbol Men Shirt",
+     image_url:"https://m.media-amazon.com/images/I/911xrpb4BGL._AC_UL480_FMwebp_QL65_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/91feHr7BH1L._AC_UY879_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/91EiDMKUUuL._AC_UY879_.jpg",
+     strikedoffprice: "799",
+     price: "557",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ 
+ {  brand_name:"Louis Philip",
+ productType:"mansclothing",
+      name:"LP Brand - Symbol Men Shirt",
+     image_url:"https://m.media-amazon.com/images/I/71ORz8flzkL._AC_UL480_QL65_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/61pO6WKIePL._AC_UY879_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/71Md5hfnoLL._AC_UY879_.jpg",
+     strikedoffprice: "799",
+     price: "557",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ 
+ {  brand_name:"Louis Philip",
+ productType:"mansclothing",
+      name:"LP Brand - Symbol Men Shirt",
+     image_url:"https://m.media-amazon.com/images/I/61UqWXWlsbL._AC_UL480_FMwebp_QL65_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/81NP3Awt2VL._AC_UY879_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/71TsJcKT6BL._AC_UY879_.jpg",
+     strikedoffprice: "799",
+     price: "557",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ 
+     {  brand_name:"NIVIA",
+     productType:"mansCream",
+      name:"Nivea Men Creme, Dark Spot Reduction, Non Greasy Moisturizer, Cream With Uv Protect, 150 ml",
+     image_url:"https://m.media-amazon.com/images/I/51NYSJRXJ6L._AC_UL400_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/51J0i8pWNmL._SY450_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/61WcUrkdpYL._SY450_.jpg",
+     strikedoffprice: "399",
+     price: "259",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"MAN MATTERS",
+    productType:"mansCream",
+      name:"Man Matters Biotin Hair Gummies | 60 Days Pack | With Biotin, |100% Vegetarian | 60 Gummies",
+     image_url:"https://m.media-amazon.com/images/I/81bt4NnbTOL._AC_UL400_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/81jL0EGMmfL._SX569_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/812po+vEY3L._SX569_.jpg",
+     strikedoffprice: "849",
+     price: "799",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"The Man Company",
+ productType:"mansCream",
+      name:"The Man Company Skin Brightening Cream with Multani Mitti, Coco Butter | Removes Blackhead - 30 gm",
+     image_url:"https://m.media-amazon.com/images/I/51QZU5qQRVL._SY450_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/71YpI+Nin1L._SY450_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/71LBwLbxtxL._SY450_.jpg",
+     strikedoffprice: "299",
+     price: "189",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"GARNIER",
+ productType:"mansCream",
+      name:"Garnier Men, Face Wash, Brightening & Anti-Pollution, TurboBright Double Action, 150 g",
+     image_url:"https://m.media-amazon.com/images/I/51YOFiC3zvL._AC_UL400_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/61pOAOfbDML._SX425_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/61qEEmhOTLL._SX425_.jpg",
+     strikedoffprice: "325",
+     price: "262",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"URBAN FOREST",
+ productType:"wallet",
+      name:"URBAN FOREST Oliver Redwood Leather Wallet for Men",
+     image_url:"https://m.media-amazon.com/images/I/71t7CgBwJ4L._AC_UL400_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/81wc7fARCsL._SY741_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/81T8LdEzLQL._SX569_.jpg",
+     strikedoffprice: "2000",
+     price: "499",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"RIGOHILL",
+ productType:"wallet",
+      name:"RIGOHILL Doger Olive Green Mens Leather Wallet | Leather Wallet for Men | RFID Mens Wallet",
+     image_url:"https://m.media-amazon.com/images/I/71SYvtX2YmL._AC_UL400_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/71lhhYRTFpL._SX569_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/81FlhNtY3NL._SX569_.jpg",
+     strikedoffprice: "2499",
+     price: "499",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"NAPA HIDE",
+ productType:"wallet",
+      name:"NAPA HIDE Leather Wallet for Men I Handcrafted I Multiple Credit | I 2 Secret Compartments",
+     image_url:"https://m.media-amazon.com/images/I/81aZAmkJQWL._AC_UL400_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/91GgbctZ1EL._UX679_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/81gZOqQXT9L._UX679_.jpg",
+     strikedoffprice: "1499",
+     price: "299",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"WildHorn",
+ productType:"wallet",
+      name:"WildHorn Trifold Leather Wallet for Men I Handcrafted I Ultra Strong Stitching | I Currency Compartment",
+     image_url:"https://m.media-amazon.com/images/I/71SJJMXG19L._AC_UL400_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/813-bVZMpIL._SX569_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/81LLM2H7xsL._SX569_.jpg",
+     strikedoffprice: "1499",
+     price: "299",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"Fastrac",
+ productType:"mansSunglasses",
+      name:"Fastrack Men Square Black Sunglasses-Pack of 1",
+     image_url:"https://m.media-amazon.com/images/I/61+THk53YIL._AC_UL400_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/619hy5yVQWL._UX522_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/61vPBNLue7L._UX679_.jpg",
+     strikedoffprice: "799",
+     price: "699",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"VELOCITY STORE",
+ productType:"mansSunglasses",
+      name:"Velocity Polarized Aviator Green INJ POL Sunglasses for Men",
+     image_url:"https://m.media-amazon.com/images/I/51TZG3rY+DL._UX679_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/51J7FIerYCL._UX679_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/518KAsYiirL._UX679_.jpg",
+     strikedoffprice: "1499",
+     price: "1299",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"Fastrack",
+ productType:"mansSunglasses",
+      name:"Fastrack Men Pilot Green Sunglasses-Pack of 1",
+     image_url:"https://m.media-amazon.com/images/I/61XFwxOh4GL._UX679_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/61X3yRvhrLL._UX679_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/71RLeDOfVEL._UX522_.jpg",
+     strikedoffprice: "2099",
+     price: "1965",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"ELEGANTE",
+ productType:"mansSunglasses",
+      name:"ELEGANTE Mens Square Sunglasses, UV Protected Stylish Sun Glasses for Men",
+     image_url:"https://m.media-amazon.com/images/I/51bzps707YL._UX679_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/71lCmJtpyHL._UX679_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/61vE4kusVTL._UX679_.jpg",
+     strikedoffprice: "2499",
+     price: "596",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"FREECULTR",
+ productType:"mansclothing",
+      name:"FREECULTR Unisex Printed Bamboo Bandana Face Mask for Dust and Sun Protection",
+     image_url:"https://m.media-amazon.com/images/I/718-a6R5nWL._UX425_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/71CS9pbsQPL._UX425_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/71xrPzGy+0L._UX425_.jpg",
+     strikedoffprice: "999",
+     price: "499",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"Lymio",
+ productType:"mansclothing",
+      name:"Lymio Casual Shirt for Men|| Shirt for Men|| Men Stylish Shirt || Men Printed Shirt (Patta)",
+     image_url:"https://m.media-amazon.com/images/I/71On2XSPuRL._UY445_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/81300Ou0hAL._UY445_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/7143xg-yZVL._UY445_.jpg",
+     strikedoffprice: "4999",
+     price: "379",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"Amazon",
+ productType:"mansclothing",
+      name:"Amazon Brand - Symbol Men Shirt",
+     image_url:"https://m.media-amazon.com/images/I/81gEEU2IeHL._UX522_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/81jMs8fB4fL._UX522_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/91yl4ysaxoL._UX522_.jpg",
+     strikedoffprice: "2099",
+     price: "557",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ },
+ {  brand_name:"Amazon",
+ productType:"mansclothing",
+      name:"Amazon Brand - Symbol Men Formal Shirt",
+     image_url:"https://m.media-amazon.com/images/I/713xbqIKW4L._UX522_.jpg",
+     image_url1:"https://m.media-amazon.com/images/I/81tuLcV5zTL._UX522_.jpg",
+     image_url2:"https://m.media-amazon.com/images/I/61PNLI3SYrL._UX522_.jpg",
+     strikedoffprice: "1699",
+     price: "836",
+     save: "save 30%",
+       extrasave : "Extra 15% OFF"
+ }
+   ]; 
+    
+ displayproduct();
+         
+   function displayproduct(){
+     mensdata.map(function(elem){
+         var div = document.createElement("div");
+ 
+     var image = document.createElement("img");
+     image.setAttribute("src" ,elem.image_url);
+     image.setAttribute("alt" ,elem.name);
+    var extrasave = document.createElement("a");
+    extrasave.textContent = elem.extrasave;
+ 
+     var name = document.createElement("h4");
+     name.textContent = elem.name;
+ var pricebox = document.createElement("div");
+ 
+     var strikedoffprice = document.createElement("p");
+     strikedoffprice.textContent = "$" + elem.strikedoffprice;
+ 
+     var price = document.createElement("p");
+     price.textContent = "$"+elem.price;
+     
+     var save = document.createElement("p");
+     save.textContent = elem.save;
+     pricebox.append(strikedoffprice,price,save);
+ 
+     
+ 
+     div.append(image,extrasave, name,pricebox);
+     document.getElementById("parent").append(div);
+     
+ 
+ 
+ 
+     image.addEventListener("click",function(){  // Added part 
+       var imgs=event.target.parentNode.children[0].getAttribute("src");
+      var discount= event.target.parentNode.children[1].innerText;
+       var discription=event.target.parentNode.children[2].innerText;
+       var rprice=event.target.parentNode.children[3].children[0].innerText;
+       console.log(rprice);
+       var price=event.target.parentNode.children[3].children[1].innerText;
+      var saving= event.target.parentNode.children[3].children[2].innerText;
+      var brand=event.target.parentNode.children[4].children[0].innerText;
+     // console.log(brand);
+      var type=event.target.parentNode.children[4].children[1].innerText;
+      var img2=event.target.parentNode.children[4].children[2].getAttribute("src");
+      var img3=event.target.parentNode.children[4].children[3].getAttribute("src");
+     // console.log();
+       var obj={};
+       obj["imgs"]=imgs;
+       obj["discount"]=discount;
+       obj["discription"]=discription;
+       obj["rprice"]=rprice;
+       obj["price"]=price;
+       obj["saving"]=saving;
+       obj["brand"]=brand;
+       obj["type"]=type;
+       obj["img2"]=img2;
+       obj["img3"]=img3;
+       localStorage.setItem("product-pge-item",JSON.stringify(obj));
+        window.location.href="product_page.html";
+ 
+        //console.log(event.target.parentNode);  
+ 
+ 
+     });
+     name.addEventListener("click",function(){  //Added part is starting
+       var imgs=event.target.parentNode.children[0].getAttribute("src");
+      var discount= event.target.parentNode.children[1].innerText;
+       var discription=event.target.parentNode.children[2].innerText;
+       var rprice=event.target.parentNode.children[3].children[0].innerText;
+       console.log(rprice);
+       var price=event.target.parentNode.children[3].children[1].innerText;
+      var saving= event.target.parentNode.children[3].children[2].innerText;
+      var brand=event.target.parentNode.children[4].children[0].innerText;
+     // console.log(brand);
+      var type=event.target.parentNode.children[4].children[1].innerText;
+      var img2=event.target.parentNode.children[4].children[2].getAttribute("src");
+      var img3=event.target.parentNode.children[4].children[3].getAttribute("src");
+     // console.log();
+       var obj={};
+       obj["imgs"]=imgs;
+       obj["discount"]=discount;
+       obj["discription"]=discription;
+       obj["rprice"]=rprice;
+       obj["price"]=price;
+       obj["saving"]=saving;
+       obj["brand"]=brand;
+       obj["type"]=type;
+       obj["img2"]=img2;
+       obj["img3"]=img3;
+       localStorage.setItem("product-pge-item",JSON.stringify(obj));
+        window.location.href="product_page.html";
+ 
+        //console.log(event.target.parentNode);  
+ 
+ 
+     });
+    
+     var mydiv=document.createElement("div");
+     mydiv.setAttribute("id","mydiv");
+     var brand=document.createElement("p");
+     brand.innerText=elem.brand_name;
+    
+     var type=document.createElement("p");
+     type.innerText=elem.productType;
+    
+     var img2=document.createElement("img");
+     img2.setAttribute("src",elem.image_url1);
+     var img3=document.createElement("img");
+     img3.setAttribute("src",elem.image_url2);
+    
+    
+     mydiv.append(brand,type,img2,img3);
+     div.append(image,extrasave, name,pricebox);
+     div.append(mydiv);
+     
+     document.getElementById("parent").append(div);
+     }) ;    
+     // till here
+ 
+   }
+ 
